b0552088325c871a53407afd4bf0abc1233ddaaf
Anton Baumann <antonbaumann4@gmail.com>
2019-07-26 15:18:24 +0200

    use testify for testing

--- stemmer.go
+++ stemmer.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	"strings"
-	"fmt"
-	"os"
-	"log"
 	"bufio"
-	"sort"
+	"fmt"
+	"log"
+	"os"
 	"regexp"
+	"sort"
+	"strings"
 )
 
 // äöü -> AOU so they can be represented as ascii chars
-const vowels 	= 	"aeiouyAOV"
-const s_ending 	= 	"bdfghklmnrt"
-const st_ending	= 	"bdfghklmnt"
+const vowels = "aeiouyAOV"
+const sEnding = "bdfghklmnrt"
+const stEnding = "bdfghklmnt"
 
-var p1, p2, x int
+var p1, p2 int
 
 func ScanFile(path string) []string {
 	var tmp []string
@@ -44,50 +44,60 @@ func step0(word string) (string, error) {
 	word = strings.Replace(word, "ü", "V", -1)
 
 	// if u,y between vowels -> ToUpper()
-	for i := 2; i<len(word); i++ {
+	for i := 2; i < len(word); i++ {
 		prev := word[i-2]
 		curr := word[i-1]
 		next := word[i]
 		if strings.Contains(vowels, string(prev)) && strings.Contains(vowels, string(next)) {
-			if curr == 'u' {word = word[:i-1] + "U" + word[i:]}
-			if curr == 'y' {word = word[:i-1] + "Y" + word[i:]}
+			if curr == 'u' {
+				word = word[:i-1] + "U" + word[i:]
+			}
+			if curr == 'y' {
+				word = word[:i-1] + "Y" + word[i:]
+			}
 		}
 	}
 	word = strings.Replace(word, "ß", "ss", -1)
 
 	var err error
 	p1, p2, err = getR(word)
-	if err != nil {return "", err}
+	if err != nil {
+		return "", err
+	}
 
 	return word, nil
 }
 
-func step1(word string) (string, error){
+func step1(word string) (string, error) {
 	R1 := ""
-	if p1 <= len(word) { R1 = word[p1:] }
+	if p1 <= len(word) {
+		R1 = word[p1:]
+	}
 
 	a := []string{"em", "ern", "er"}
 	b := []string{"e", "en", "es"}
 
 	for _, suffix := range a {
-		if strings.HasSuffix(R1, suffix){
+		if strings.HasSuffix(R1, suffix) {
 			word = word[:len(word)-len(suffix)]
 			return word, nil
 		}
 	}
 	for _, suffix := range b {
-		if strings.HasSuffix(R1, suffix){
+		if strings.HasSuffix(R1, suffix) {
 			word = word[:len(word)-len(suffix)]
-			if strings.HasSuffix(word, "niss"){
+			if strings.HasSuffix(word, "niss") {
 				word = word[:len(word)-1]
 			}
 			return word, nil
 		}
 	}
 	if strings.HasSuffix(word, "s") {
-		rune, err := RuneAt(word,len(word)-2)
-		if err != nil {return "", err}
-		if strings.Contains(s_ending, string(rune)) {
+		rune, err := RuneAt(word, len(word)-2)
+		if err != nil {
+			return "", err
+		}
+		if strings.Contains(sEnding, string(rune)) {
 			word = word[:len(word)-1]
 		}
 	}
@@ -97,19 +107,21 @@ func step1(word string) (string, error){
 
 func step2(word string) string {
 	R1 := ""
-	if p1 <= len(word) { R1 = word[p1:] }
+	if p1 <= len(word) {
+		R1 = word[p1:]
+	}
 
 	a := []string{"an", "er", "est"}
 
 	for _, suffix := range a {
-		if strings.HasSuffix(R1, suffix){
+		if strings.HasSuffix(R1, suffix) {
 			word = word[:len(word)-len(suffix)]
 			return word
 		}
 	}
 
-	if strings.HasSuffix(R1, "st") && len(word) >= 6{
-		if strings.Contains(st_ending, string(word[len(word)-3])) {
+	if strings.HasSuffix(R1, "st") && len(word) >= 6 {
+		if strings.Contains(stEnding, string(word[len(word)-3])) {
 			word = word[:len(word)-2]
 		}
 	}
@@ -118,9 +130,13 @@ func step2(word string) string {
 
 func step3(word string) string {
 	R1 := ""
-	if p1 <= len(word) { R1 = word[p1:] }
+	if p1 <= len(word) {
+		R1 = word[p1:]
+	}
 	R2 := ""
-	if p2 <= len(word) { R2 = word[p2:] }
+	if p2 <= len(word) {
+		R2 = word[p2:]
+	}
 	a := []string{"end", "ung"}
 	b := []string{"ig", "ik", "isch"}
 	c := []string{"lich", "heit"}
@@ -128,10 +144,12 @@ func step3(word string) string {
 
 	for _, suffix := range a {
 		// On website they use R2
-		if strings.HasSuffix(R1, suffix){
+		if strings.HasSuffix(R1, suffix) {
 			word = word[:len(word)-len(suffix)]
 			R1 = ""
-			if p1 <= len(word) { R1 = word[p1:] }
+			if p1 <= len(word) {
+				R1 = word[p1:]
+			}
 			if strings.HasSuffix(R2, "ig") {
 				i := len(word) - 3
 				if i >= 0 && word[i] == 'e' {
@@ -143,7 +161,7 @@ func step3(word string) string {
 	}
 
 	for _, suffix := range b {
-		if strings.HasSuffix(R2, suffix){
+		if strings.HasSuffix(R2, suffix) {
 			i := len(word) - 3
 			if i >= 0 && word[i] != 'e' {
 				word = word[:len(word)-len(suffix)]
@@ -153,10 +171,12 @@ func step3(word string) string {
 	}
 
 	for _, suffix := range c {
-		if strings.HasSuffix(R2, suffix){
+		if strings.HasSuffix(R2, suffix) {
 			word = word[:len(word)-len(suffix)]
 			R2 = ""
-			if p2 <= len(word) { R2 = word[p2:] }
+			if p2 <= len(word) {
+				R2 = word[p2:]
+			}
 			if strings.HasSuffix(R1, "er") || strings.HasSuffix(R1, "en") {
 				word = word[:len(word)-2]
 			}
@@ -165,13 +185,15 @@ func step3(word string) string {
 	}
 
 	for _, suffix := range d {
-		if strings.HasSuffix(R2, suffix){
+		if strings.HasSuffix(R2, suffix) {
 			word = word[:len(word)-len(suffix)]
 			R2 = ""
-			if p2 <= len(word) { R2 = word[p2:] }
-			if strings.HasSuffix(R2, "lich"){
+			if p2 <= len(word) {
+				R2 = word[p2:]
+			}
+			if strings.HasSuffix(R2, "lich") {
 				word = word[:len(word)-4]
-			} else if strings.HasSuffix(R2,"ig") {
+			} else if strings.HasSuffix(R2, "ig") {
 				word = word[:len(word)-2]
 			}
 			return word
@@ -204,48 +226,63 @@ func getR(word string) (int, int, error) {
 	//	}
 	//}
 	//if p1 == 0 || p1 >= len(word){
-		for i := 1; i<len(word); i++ {
-			prev, err := RuneAt(word, i-1)
-			if err != nil {return -1, -1, err}
-			pos, err := RuneAt(word, i)
-			if err != nil {return -1, -1, err}
-			if strings.ContainsRune(vowels, prev) && !strings.ContainsRune(vowels, pos) {
-				p1 = i+1
-				break
-			}
-		}
-	//}
-	for i := p1+1; i<len(word); i++ {
+	for i := 1; i < len(word); i++ {
 		prev, err := RuneAt(word, i-1)
-		if err != nil {return -1, -1, err}
+		if err != nil {
+			return -1, -1, err
+		}
 		pos, err := RuneAt(word, i)
-		if err != nil {return -1, -1, err}
+		if err != nil {
+			return -1, -1, err
+		}
 		if strings.ContainsRune(vowels, prev) && !strings.ContainsRune(vowels, pos) {
-			p2 = i+1
+			p1 = i + 1
 			break
 		}
 	}
-	if p2 == 0 {p2 = len(word)}
-	if p1 == 0 {p1 = len(word)}
+	//}
+	for i := p1 + 1; i < len(word); i++ {
+		prev, err := RuneAt(word, i-1)
+		if err != nil {
+			return -1, -1, err
+		}
+		pos, err := RuneAt(word, i)
+		if err != nil {
+			return -1, -1, err
+		}
+		if strings.ContainsRune(vowels, prev) && !strings.ContainsRune(vowels, pos) {
+			p2 = i + 1
+			break
+		}
+	}
+	if p2 == 0 {
+		p2 = len(word)
+	}
+	if p1 == 0 {
+		p1 = len(word)
+	}
 
 	return p1, p2, nil
 }
 
 func RuneAt(string string, int int) (rune, error) {
 	if int >= len(string) {
-		err := fmt.Errorf("Index out of bound: %v (length is %v)", int, len(string))
+		err := fmt.Errorf("index out of bound: %v (length is %v)", int, len(string))
 		return -1, err
 	}
-
 	return []rune(string)[int], nil
 }
 
 func Stem(word string) (string, error) {
 	var err error
 	word, err = step0(word)
-	if err != nil {return "", err}
+	if err != nil {
+		return "", err
+	}
 	word, err = step1(word)
-	if err != nil {return "", err}
+	if err != nil {
+		return "", err
+	}
 	word = step2(word)
 	word = step3(word)
 	word = step4(word)
@@ -257,15 +294,15 @@ func StemQuery(query string) (string, error) {
 	sort.Strings(stopWords)
 	r := regexp.MustCompile("[^\\s]+")
 	words := r.FindAllString(query, -1)
-	var new_query = ""
+	newQuery := ""
 	for _, w := range words {
 		if stopWords[sort.SearchStrings(stopWords, w)] != w {
 			res, err := Stem(w)
 			if err != nil {
 				return "", err
 			}
-			new_query += res + " "
+			newQuery += res + " "
 		}
 	}
-	return strings.TrimSpace(new_query), nil
+	return strings.TrimSpace(newQuery), nil
 }
--- stemmer_test.go
+++ stemmer_test.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-	"testing"
 	"fmt"
+	"github.com/stretchr/testify/assert"
 	"strings"
+	"testing"
 )
 
-
 func TestStem(t *testing.T) {
-	var words 				= ScanFile("txt/words_to_stem.txt")
-	var correctly_stemmed 	= ScanFile("txt/correctly_stemmed_words.txt")
-	var stemmed 			[]string
+	words := ScanFile("txt/words_to_stem.txt")
+	correctlyStemmed := ScanFile("txt/correctly_stemmed_words.txt")
+	var stemmed []string
 
 	for _, w := range words {
-		//fmt.Println("testing " + w)
 		st, err := Stem(w)
 		if err != nil {
 			t.Error(err)
@@ -22,41 +21,16 @@ func TestStem(t *testing.T) {
 	}
 
 	for i, w := range words {
-		fmt.Printf("%-25s %-20s %-3t\n", w, stemmed[i], strings.EqualFold(correctly_stemmed[i], stemmed[i]))
-		if !strings.EqualFold(correctly_stemmed[i], stemmed[i]) {
-			fmt.Println("! ", correctly_stemmed[i])
-		}
+		fmt.Printf("%-25s %-20s %-3t\n", w, stemmed[i], strings.EqualFold(correctlyStemmed[i], stemmed[i]))
+		assert.Equal(t, correctlyStemmed[i], stemmed[i])
 	}
-}
-
-func TestStem2(t *testing.T) {
-
 }
 
 func TestStemQuery(t *testing.T) {
-	queries := []string{
-		"wie kann ich schnell abnehmen",
+	query := "wie kann ich schnell abnehmen"
+	result, err := StemQuery(query)
+	if err != nil {
+		t.Fatal(err)
 	}
-	for _, query := range queries {
-		fmt.Println(StemQuery(query))
-	}
+	assert.Equal(t, "schnell abnehm", result)
 }
-
-//func TestStem2(t *testing.T) {
-//	word := "kätzchens"
-//	var err error
-//	word0, err := step0(word)
-//	if err != nil {log.Println(err)}
-//	fmt.Println(0, word0)
-//	word1, err := step1(word0)
-//	if err != nil {log.Println(err)}
-//	fmt.Println(1, word1)
-//	word2 := step2(word1)
-//	fmt.Println(2, word2)
-//	word3 := step3(word2)
-//	fmt.Println(3, word3)
-//	word4 := step4(word3)
-//	fmt.Println(4, word4)
-//	fmt.Println(word4)
-//}
-
